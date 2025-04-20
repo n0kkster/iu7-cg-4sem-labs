ebb22d7375d144f3dd9e6cc23d32603b06a7ca34
n0kkster <order.graph@gmail.com>
2025-04-21 02:05:03 +0300

    fix: поправил название функции

--- lab_06/inc/fill.h
+++ lab_06/inc/fill.h
@@ -8,7 +8,7 @@
 #include <QVector>
 
 
-void fill2(QPainter &painter, const QImage &buffer, const point_t seed, const QColor &border_color,
-           const QColor &fill_color);
+int fill(QPainter &painter, const QImage &buffer, const point_t seed, const QColor &border_color,
+           const QColor &fill_color, int iter_stop = -1);
 
 #endif /* FILL_H */
